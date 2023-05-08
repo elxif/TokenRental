7cd9475f83f0b140bbe284de662ead17090bed6f
Elif Akalın <elifnurakalin@gmail.com>
2023-05-08 22:48:57 +0300

    member struct added

--- TokenRental.sol
+++ TokenRental.sol
@@ -5,12 +5,14 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract TokenRentalSystem is ERC721 {
 
     address public owner;
-    address public renter;
 
     constructor() ERC721("TokenRental", "TRS") {
         owner = msg.sender;
     }
 
+    struct Member {
+        string Name;
+    }
+
 }
-
-
+ 
