659613e1222899e53695ddd69a5dcbafc9265975
serdarakol <serdar@DESKTOP-PT5M6QP.localdomain>
2023-05-08 22:37:20 +0300

    renter field added

--- TokenRental.sol
+++ TokenRental.sol
@@ -5,10 +5,12 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract TokenRentalSystem is ERC721 {
 
     address public owner;
+    address public renter;
 
     constructor() ERC721("TokenRental", "TRS") {
         owner = msg.sender;
     }
 
 }
- 
+
+
